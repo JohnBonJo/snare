d9114ef85b071d73ddb970b1d4c212157f3962d1
JohnBonJo <74373209+JohnBonJo@users.noreply.github.com>
2022-07-24 18:10:01 +0300

    Update cloner.py

--- snare/cloner.py
+++ snare/cloner.py
@@ -175,8 +175,12 @@ class Cloner(object):
                     soup = await self.replace_links(data, level)
                     data = str(soup).encode()
                 elif content_type == "text/css":
-                    #css = cssutils.parseString(data, validate=self.css_validate)
-                    css = cssutils.parseString(data, validate=False)
+                    if self.css_validate == "False":
+                        css = cssutils.parseString(data, validate=False)
+                    else:
+                        css = cssutils.parseString(data)
+                    # css = cssutils.parseString(data, validate=self.css_validate)
+                    # css = cssutils.parseString(data, validate=False)
                     for carved_url in cssutils.getUrls(css):
                         if carved_url.startswith("data"):
                             continue
